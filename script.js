b03dbb453a4933573f632513d9e63ffda731c4f7
oxfordblucher <hiuchanhk@gmail.com>
2020-09-30 12:31:44 -0400

    timeblocks are now created dynamically in js

--- script.js
+++ script.js
@@ -4,29 +4,61 @@ var lead = $(".lead");
 var today = $("#currentDay");
 var container = $(".container");
 
-var tSpan = $("<span>");
-tSpan.text("Today's date: ");
-tSpan.attr("class", "badge badge-info");
 
-today.prepend(tSpan);
-today.append(moment().format("LL"));
-
-var button = $("button");
-
-button.on("click", function() {
-    var disBtn = $(this).attr("data-value");
-    var disWrk = $(disBtn).val();
-    localStorage.setItem(disBtn, disWrk);
-})
-
-
-var workHours = ["#8am", "#9am", "#10am", "#11am", "#12noon", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm", "6pm"];
-
-var timeArr=[8,9,10,11,12,13,14,15,16,17];
-
-$("#currentDay").text(moment().format("LLL"));
-var currentTime=moment().format("HH")
+today.text(moment().format("LLL"));
+var currentTime = moment().format("HH")
 console.log(currentTime)
+
+var timeArr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+jQuery.each(timeArr, function(i, val) {
+    var d1 = $("<div class = 'row border rounded' style = 'height: 150px'>");
+    var d2 = $("<div class = 'col-1'>");
+    var d3 = $("<div class = 'col-9'>");
+    var d4 = $("<div class = 'col-2'>");
+    var nuTxt = $("<textarea rows = '5' class = 'saved-events form-control'>");
+    var btn = $("<button class = 'button button-primary'>");
+
+    var civvyTime = function isPM(a, b) {
+        if(timeArr[i] < 12) {
+            return timeArr[i] + " am";
+        }else if(timeArr[i] === 12) {
+            return timeArr[i] + " pm";
+        }else if(timeArr[i] > 12) {
+            return timeArr[i] - 12 + " pm";
+        }}
+
+    d1.attr("data-value", timeArr[i]);
+    d1.attr("id", timeArr[i]);
+
+    d2.text(civvyTime);
+
+    nuTxt.attr("id", timeArr[i]);
+    nuTxt.attr("data-value", timeArr[i]);
+
+    btn.attr("data-value", timeArr[i]);
+    btn.text("Save")
+
+    container.append(d1);
+    d1.append(d2);
+    d2.after(d3);
+    d3.after(d4);
+    d3.append(nuTxt);
+    d4.append(btn);
+});
+
+
+
+//present
+//currentTime == timeArr[i]
+/* textarea.attr("class","saved-events form control present")
+
+//past
+//currentTime-timeArr[i]>0
+textarea.attr("class","saved-events form control past")
+
+//future
+textarea.attr("class","saved-events form control future") */
 
 //1. create what you want via html snip
 
@@ -71,3 +103,9 @@ var d5= $("<div>");
 //5. append to html
 */
 
+//$(".submit").onclick(function(){
+    /*
+   var currentHr= $(this).attr("data-val"); //9
+  var userinput= $(this).parent().val(); 
+  */
+  //sfasdfas
