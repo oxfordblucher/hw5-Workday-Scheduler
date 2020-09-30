f74faf4f3e482b0bedb28ba02580f57de49e07ad
oxfordblucher <hiuchanhk@gmail.com>
2020-09-30 17:28:49 -0400

    Added refresh function to buttons, Added clear localStorage button, Added color coding.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ This is the unit 5 homework for UPenn LPS Coding Bootcamp.
 For this assignment, I am to create a functioning and dynamic daily planner. I assume I am to 
 familiarize myself with jQuery and moment.js in order to succeed in this endeavor.
 _____________________________________________________________________________________________________
+
+![image](assets/screenshot_working timeblocks.png "Dynamic timeblocks")
+
+![image](assets/screenshot_working localStorage.png "Working localstorage")
--- script.js
+++ script.js
@@ -4,20 +4,24 @@ var lead = $(".lead");
 var today = $("#currentDay");
 var container = $(".container");
 
+$("#clear").on("click", function() {
+    localStorage.clear();
+    location.reload();
+    return false;
+})
+
 today.text(moment().format("LLL"));
 var currentTime = moment().format("HH");
-console.log(localStorage);
-console.log(localStorage.getItem("9"));
 
 var timeArr = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
 jQuery.each(timeArr, function(i, val) {
-    var d1 = $("<div class = 'row border rounded' style = 'height: 150px'>");
-    var d2 = $("<div class = 'col-1'>");
+    var d1 = $("<div class = 'row'>");
+    var d2 = $("<div class = 'col-1 hour'>");
     var d3 = $("<div class = 'col-9'>");
     var d4 = $("<div class = 'col-2'>");
-    var nuTxt = $("<textarea rows = '5' class = 'saved-events form-control'>");
-    var btn = $("<button class = 'button button-primary'>");
+    var nuTxt = $("<textarea class = 'saved-events'>");
+    var btn = $("<button class = 'saveBtn'>");
 
     var civvyTime = function isPM(a, b) {
         if(timeArr[i] < 12) {
@@ -37,7 +41,7 @@ jQuery.each(timeArr, function(i, val) {
     nuTxt.attr("data-value", timeArr[i]);
 
     btn.attr("data-value", timeArr[i]);
-    btn.text("Save")
+    btn.text("Save Task(s)");
 
     container.append(d1);
     d1.append(d2);
@@ -47,12 +51,23 @@ jQuery.each(timeArr, function(i, val) {
     d4.append(btn);
 
     if(localStorage.getItem(timeArr[i]) !== null) {
-        nuTxt.attr("placeholder", localStorage.getItem(timeArr[i]));
+        nuTxt.text(localStorage.getItem(timeArr[i]));
+    }
+
+    if(timeArr[i] > currentTime) {
+        d1.addClass("future");
+    }else if(timeArr[i] == currentTime) {
+        d1.addClass("present");
+    }else if(timeArr[i] < currentTime) {
+        d1.addClass("past");
     }
 });
 
-$(".button").on("click", function() {
+$(".saveBtn").on("click", function() {
     var currentHr = $(this).attr("data-value");
     var userInput = $(this).parent().siblings().children('.saved-events').val();
     localStorage.setItem(currentHr, userInput);
+    location.reload();
+    return false;
 });
+
